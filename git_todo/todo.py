8d79e37bb4cafbb10da07d2ca191a89193574085
Marc Brinkmann <git@marcbrinkmann.de>
2015-08-21 10:16:22 +0200

    Initial import.

---
+++ git_todo/todo.py
@@ -0,0 +1,3 @@
+class TODOBranch(object):
+    def __init__(self):
+        pass
---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='git-todo',
+    version='0.1.dev1',
+    description=
+    'Tracks issues and TODOs in a standalone branch in your repository.',
+    long_description=read('README.rst'),
+    author='Marc Brinkmann',
+    author_email='git@marcbrinkmann.de',
+    url='https://github.com/mbr/git-todo',
+    license='MIT',
+    packages=find_packages(exclude=['tests']),
+    install_requires=['dulwich'],
+    entry_points={
+        'console_scripts': [
+            'git-todo = git_todo:cli.cli',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ])
